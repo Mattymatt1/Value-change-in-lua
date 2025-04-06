df3ba9a88846c198d7d378c176719ec5274cf8dc
Mattymatt1 <166906010+Mattymatt1@users.noreply.github.com>
2025-04-06 18:37:44 -0500

    Update valuechanginginlua.lua

--- valuechanginginlua.lua
+++ valuechanginginlua.lua
@@ -1,14 +1,14 @@
+variable definition:
 a = 5
+a2 = a*a
+-- end
 
-Multiply = function()
-local a2 = a*a
-end
 
 if a <= 5 then
   a == 5
 end
 
- for i = 1, a do
+ for i = 1, a2 do
 print("*")
 if a % 2 == 1 then
      a = a - 1
